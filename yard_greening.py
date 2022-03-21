2aa26d3c88d36dff10a0ff85579d81f0db8f8cdb
Mihail Polimenov <Mistpolimen91@gmail.com>
2022-03-21 23:26:26 +0200

    Delete yard_greening.py

--- yard_greening.py
+++
@@ -1,6 +0,0 @@
-area = float(input())
-total_price = area * 7.61
-discount = total_price * 0.18
-end_price = total_price - discount
-print(f"The final price is: {end_price} lv.")
-print(f'The discount is: {discount} lv.')
