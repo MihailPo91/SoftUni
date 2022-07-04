fc4cdb51b0bc40af702bd202fa8e1bef872f4f18
Mihail Polimenov <Mistpolimen91@gmail.com>
2022-07-05 01:07:26 +0300

    Add files via upload

---
+++ OOP/Inheritance/lab/Stack_of_Strings/project/stack.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class Stack:
+    def __init__(self):
+        self.data = list()
+
+    def push(self, element: str):
+        self.data.append(str(element))
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return f'[{", ".join(reversed(self.data))}]'
+
+
+class StackTests(unittest.TestCase):
+    def test_zero(self):
+        stack = Stack()
+        stack.push("apple")
+        stack.push("carrot")
+        self.assertEqual(str(stack), '[carrot, apple]')
+        self.assertEqual(stack.pop(), 'carrot')
+        self.assertEqual(stack.top(), 'apple')
+        stack.push("cucumber")
+        self.assertEqual(str(stack), '[cucumber, apple]')
+        self.assertEqual(stack.is_empty(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
